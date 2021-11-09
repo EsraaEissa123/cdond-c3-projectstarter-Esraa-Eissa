5417e6b7db829f5fd69c32f5813c378e9c85c795
esraa_eissa123 <esraa.eissa123@gmail.com>
2021-11-09 21:03:35 +0200

    commit

--- backend/src/main.hmr.ts
+++ backend/src/main.hmr.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'modules/app/app.module';
 import { ConfigService } from './modules/config/config.service';
 
-declare const module: any; 
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
